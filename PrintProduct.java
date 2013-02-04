66735d11ea2831360b5a663e73265c3f2828d7a9
Hans-Peter Höllwirth <hans-peter.hoellwirth@gmx.at>
2013-02-04 15:59:33 +0000

    dummy change

--- PrintProduct.java
+++ PrintProduct.java
@@ -2,24 +2,25 @@
 /* A trivial console applet that reads two
    numbers and prints their product.
 */
+// dummy change by Hans-Peter Hoellwirth
 
 public class PrintProduct extends ConsoleApplet {
-   
+
    public void program() {
-   
+
       double x,y;   // Numbers input by the user.
       double prod;  // The product, x*y.
-      
+
       console.put("What is your first number?  ");
       x = console.getlnDouble();
       console.put("What is your second number? ");
       y = console.getlnDouble();
-      
+
       prod = x * y;
       console.putln();
       console.put("The product is ");
       console.putln(prod);
-      
+
    } // end program()
-   
+
 } // end class PrintProduct
